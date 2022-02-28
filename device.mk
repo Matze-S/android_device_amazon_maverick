a5a210f20290de44cb7244e7406868669f7201f3
Vaisakh Murali <mvaisakh@statixos.com>
2022-02-28 13:29:20 +0000

    maverick: Switch to OSS audio HAL service Prebuilt audio HAL depends on a lot of VNDK 28 dependencies, using it as is requires a lot of hacks. Switching to OSS HAL does not require hexediting any prebuilt blobs and their depdendencies.

--- device.mk
+++ device.mk
@@ -65,6 +65,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio.service \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     libtinyxml
