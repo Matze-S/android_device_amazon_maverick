6a745b166aec5caa31f5cecb0dd56c1c1862ee2f
TheMalachite <eliasgheeraert@gmail.com>
2022-02-28 13:09:48 +0000

    maverick: Build libtinyxml from source

--- device.mk
+++ device.mk
@@ -59,7 +59,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.1-impl \
+    libtinyxml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
