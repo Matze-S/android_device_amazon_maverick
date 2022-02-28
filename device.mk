4b11a4f0f46d1d75eda74b79bb289f6e6fdafa65
daniml3 <danimoral1001@gmail.com>
2022-02-28 20:17:32 +0000

    maverick: build the DRM clearkey plugin from source

--- device.mk
+++ device.mk
@@ -115,7 +115,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.3-service.clearkey \
+    libdrmclearkeyplugin \
+    libmockdrmcryptoplugin
 
 # Permissions
 PRODUCT_COPY_FILES += \
